e89867b7297f44fbc3fcfa6acf4ed8afb5367491
"U-AD4\pferdina" <pferdina@cv0013271n0>
2010-06-30 23:21:31 +0800

    code_gen.py does a little; re-arrange my_cfg.cpp in logical order.

--- code_gen/code_gen.py
+++ code_gen/code_gen.py
@@ -10,16 +10,75 @@ Usage: python code_gen.py [source XML file]
 from xml.dom import minidom
 import sys
 
+def get_child_elements(parent):
+    """Return a list of child nodes that are elements, not text (incl carriage returns) or comments."""
+    return [node for node in parent.childNodes if node.nodeType == minidom.Node.ELEMENT_NODE]
+
+
+def process_item(item, prefix):
+    """Process XML element with tag 'item'"""
+    print "process_item", item, "PREFIX",prefix
+    aggr_list = []
+    # For all the elements within the item: name, ID, aggregates(s), etc...
+    for element in get_child_elements(item):
+        print "TAGNAME", element.tagName
+        # Assume each item has at least one "name" child element
+        if (element.tagName == "name"):
+            nameText = [node.data for node in element.childNodes if node.nodeType == minidom.Node.TEXT_NODE][0]
+            print "NAMETEXT", nameText
+
+        # print element.toxml()
+        if (element.tagName == "aggregate"):
+           print "NAMETEXT2", nameText
+           newPrefix = prefix + nameText
+           print "NEWPREFIX", newPrefix
+           aggr_list.append(process_aggregate(element, newPrefix))
+           
+    print "NAMETEXT3", nameText
+            
+    if len(aggr_list) > 0:
+        # Composite item
+        for aggr in aggr_list:
+            # list of aggregates in this composite item
+            fout.write(aggr)
+            fout.write(",")
+            fout.write("\n")
+        desc_init = "const cm_composite_item_descriptor " + prefix + nameText + " = cm_composite_item_descriptor\n"
+        desc_init += "(\n\"" + nameText + "\",\n"
+    else:
+        # Simple item
+        desc_init = "const cm_basic_item_descriptor " + prefix + nameText + " = cm_basic_item_descriptor\n"
+        desc_init += "(\n\"" + nameText + "\",\n"
+    
+    desc_init += ");\n"
+    fout.write(desc_init)
+    return nameText
+
+
+def process_aggregate(aggr, prefix):
+    print "process_aggregate", aggr, prefix
+    """Process XML element with tag 'aggregate'"""
+    # Assume each aggregate has at least 1 "item" child element
+    item = [node for node in get_child_elements(aggr) if node.tagName == "item"][0]
+    item_name = process_item(item, prefix)
+    
+    aggr_name = item_name + "_aggregate"
+    aggr_init = aggr_name + ";\n"
+    fout.write(aggr_init)
+    return aggr_name
+
 
 f = open(sys.argv[1])
+outfname = "out.cpp"
+fout = open(outfname, "w")
 print "Reading", sys.argv[1]
 
 xmldoc = minidom.parse(f).documentElement
 
-# We're only interested in the child elements, not the other nodes like text (including
-# carriage returns) and comments.
-for element in [node for node in xmldoc.childNodes if node.nodeType == minidom.Node.ELEMENT_NODE]:
-    print element.toxml()
-
+if (xmldoc.tagName == "item"):
+    process_item(xmldoc, "device")
+else:
+    print "Document has", xmldoc, "at top level"
 
 f.close()
+fout.close()
--- my_cfg.cpp
+++ my_cfg.cpp
@@ -20,27 +20,9 @@ using namespace std;
 // It may also be that objects with virtual function are not ROMable (where is the vtable and
 // when is it initialized?).
 
+// xxx should be a naming convention: camelCase, or underscores, etc.
 
-// Allocate item IDs, which are unique within their contexts.
-// These are saved to NVRAM.
-enum
-{
-    USER_NAME = 0,
-    USER_ID = 1,
-    USER_TEMP = 2,
-};
-
-enum
-{
-    HOME_LOC_NAME = 0,
-};
-
-enum
-{
-    HOME_ADDR = 0,
-    HOME_LOC = 1,
-};
-
+/* Start of composite item device */
 enum
 {
     IPADDR  = 0,
@@ -51,14 +33,14 @@ enum
     
 };
 
-// xxx should be a naming convention: camelCase, or underscores; order in which things are defined, etc.
-
 const cm_basic_item_descriptor ip_address = cm_basic_item_descriptor("ipaddr",
                                                                  IPADDR,
                                                                  sizeof(unsigned long), // xxx define SIZEOF macro
                                                                  cm_set_int,
                                                                  NULL, // setdef
                                                                  cm_prt_int);
+                                                                 
+const cm_contained_aggregate ipaddressAggr(&ip_address, 1, offsetof(tDevice, addr));
 
 const cm_basic_item_descriptor port = cm_basic_item_descriptor("port", 
                                                             CLIPORT,
@@ -66,11 +48,23 @@ const cm_basic_item_descriptor port = cm_basic_item_descriptor("port",
                                                             cm_set_int,
                                                             NULL, // setdef
                                                             cm_prt_int);
+                                                            
+const cm_contained_aggregate portAggr(&port, NUM_CLI_PORT, offsetof(tDevice, cliPort));
 
 const cm_cntr_item_descriptor userCnt = cm_cntr_item_descriptor("usercnt", 
                                                               USERCOUNT,
                                                               sizeof(unsigned int), // xxx
                                                               cm_prt_int);
+                                                              
+const cm_contained_aggregate userCntAggr = cm_contained_aggregate(&userCnt, 1, offsetof(tDevice, userCount));
+
+/* Start of composite item user */
+enum
+{
+    USER_NAME = 0,
+    USER_ID = 1,
+    USER_TEMP = 2,
+};
 
 const cm_basic_item_descriptor user_name = cm_basic_item_descriptor("name", 
                                                                USER_NAME,
@@ -78,6 +72,8 @@ const cm_basic_item_descriptor user_name = cm_basic_item_descriptor("name",
                                                                cm_set_str,
                                                                NULL, // setdef
                                                                cm_prt_str);
+                                                               
+const cm_contained_aggregate userNameAggr(&user_name, 1, offsetof(tUser, name));                                                               
 
 const cm_basic_item_descriptor user_id = cm_basic_item_descriptor("id", 
                                                              USER_ID,
@@ -85,6 +81,8 @@ const cm_basic_item_descriptor user_id = cm_basic_item_descriptor("id",
                                                              cm_set_int, // xxx unsigned
                                                              NULL, // setdef
                                                              cm_prt_int /* xxx unsigned */);
+                                                             
+const cm_contained_aggregate userIdAggr(&user_id, 1, offsetof(tUser, id));
 
 const cm_basic_item_descriptor user_temp = cm_basic_item_descriptor("temp", 
                                                                USER_TEMP,
@@ -92,29 +90,10 @@ const cm_basic_item_descriptor user_temp = cm_basic_item_descriptor("temp",
                                                                cm_set_int, // xxx set
                                                                setdef_temp, // setdef
                                                                cm_prt_int);
-
-const cm_basic_item_descriptor home_addr = cm_basic_item_descriptor("addr", 
-                                                               HOME_ADDR,
-                                                               sizeof(unsigned long), // xxx
-                                                               cm_set_int, // xxx set
-                                                               NULL, // setdef
-                                                               cm_prt_int);
-
-const cm_basic_item_descriptor home_loc_name = cm_basic_item_descriptor("name", 
-                                                               HOME_LOC_NAME,
-                                                               MAX_LEN_LOCATION_NAME, // xxx
-                                                               cm_set_str,
-                                                               NULL, // setdef
-                                                               cm_prt_str);
-
-/* We define this one separately because we want to reference it in two places */
-const cm_contained_aggregate userCntAggr = cm_contained_aggregate(&userCnt, 1, offsetof(tDevice, userCount));
-
-
-const cm_contained_aggregate userNameAggr(&user_name, 1, offsetof(tUser, name));
-const cm_contained_aggregate userIdAggr(&user_id, 1, offsetof(tUser, id));
+                                                               
 const cm_contained_aggregate userTempAggr(&user_temp, 1, offsetof(tUser, temperature));
 
+/* List of aggregates in user */
 const cm_aggregate * const userAggrList[] = 
 {
     &userNameAggr,
@@ -128,19 +107,47 @@ const cm_composite_item_descriptor user = cm_composite_item_descriptor(
     sizeof(tUser),
     userAggrList,
     sizeof(userAggrList)/sizeof(userAggrList[0])
-);
+);                                             
 
+const cm_owned_aggregate userAggr(&user, 3 /* xxx max number of users */, offsetof(tDevice, users), &userCntAggr);
 
-const cm_contained_aggregate homeLocNameAggr(&home_loc_name, 1, offsetof(tLocation, name));
+/* Start of composite item home */
+enum
+{
+    HOME_ADDR = 0,
+    HOME_LOC = 1,
+};
 
+const cm_basic_item_descriptor home_addr = cm_basic_item_descriptor("addr", 
+                                                               HOME_ADDR,
+                                                               sizeof(unsigned long), // xxx
+                                                               cm_set_int, // xxx set
+                                                               NULL, // setdef
+                                                               cm_prt_int);
+                                                               
 // The last param is NULL because the owned item has no corresponding counter
 const cm_owned_aggregate homeAddrAggr(&home_addr, 1, offsetof(tHome, pAddr), NULL);
 
+/* Start of composite item home location */
+enum
+{
+    HOME_LOC_NAME = 0,
+};
+
+const cm_basic_item_descriptor home_loc_name = cm_basic_item_descriptor("name", 
+                                                               HOME_LOC_NAME,
+                                                               MAX_LEN_LOCATION_NAME, // xxx
+                                                               cm_set_str,
+                                                               NULL, // setdef
+                                                               cm_prt_str);
+
+const cm_contained_aggregate homeLocNameAggr(&home_loc_name, 1, offsetof(tLocation, name));
+
+/* List of aggregates in home_loc */
 const cm_aggregate * const homeLocAggrList[] = 
 {
     &homeLocNameAggr,
 };
-
 
 const cm_composite_item_descriptor home_loc = cm_composite_item_descriptor(
     "loc", 
@@ -153,6 +160,7 @@ const cm_composite_item_descriptor home_loc = cm_composite_item_descriptor(
 // The last param is NULL because the owned item has no corresponding counter
 const cm_owned_aggregate homeLocAggr(&home_loc, 1, offsetof(tHome, pLoc), NULL);
 
+/* List of aggregates in home */
 const cm_aggregate * const homeAggrList[] = 
 {
     &homeAddrAggr,
@@ -167,13 +175,9 @@ const cm_composite_item_descriptor home = cm_composite_item_descriptor(
     sizeof(homeAggrList)/sizeof(homeAggrList[0])
 );
 
-
-
-const cm_contained_aggregate ipaddressAggr(&ip_address, 1, offsetof(tDevice, addr));
-const cm_contained_aggregate portAggr(&port, NUM_CLI_PORT, offsetof(tDevice, cliPort));
-const cm_owned_aggregate userAggr(&user, 3 /* xxx max number of users */, offsetof(tDevice, users), &userCntAggr);
 const cm_contained_aggregate homeAggr(&home, 1 , offsetof(tDevice, home));
 
+/* List of aggregates in device */
 const cm_aggregate * const deviceAggrList[] = 
 {
     &ipaddressAggr,
@@ -193,4 +197,3 @@ const cm_composite_item_descriptor deviceDesc = cm_composite_item_descriptor(
 // A pointer to the base descriptor: a global used by the application code
 // to initialize the config manager xxx
 const cm_item_descriptor * pBaseDesc = &deviceDesc;
-
